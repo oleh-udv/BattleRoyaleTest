9a9ed71d63ab6a5bc55d47ea1f7efe425f3c9894
oleh <udovychenko.oleh@gmail.com>
2026-04-02 16:42:56 +0300

    Start setup project and zenject

---
+++ Assets/_ROOT/Scripts/GameSceneInstaller.cs
@@ -0,0 +1,12 @@
+namespace Scripts
+{
+    using Zenject;
+
+    public class GameSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
